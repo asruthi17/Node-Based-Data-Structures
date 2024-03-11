efe49e8bd2843ad57e161c462fb2cf18013b8a6e
asruthi17 <151494070+asruthi17@users.noreply.github.com>
2024-03-11 12:15:46 +0530

    Create Merge Nodes in Between Zeros.py

---
+++ Merge Nodes in Between Zeros.py
@@ -0,0 +1,31 @@
+'''You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+
+For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+
+Return the head of the modified linked list.
+
+Input: head = [0,3,1,0,4,5,2,0]
+Output: [4,11]
+Explanation: 
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 3 + 1 = 4.
+- The sum of the nodes marked in red: 4 + 5 + 2 = 11.'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        d1=ListNode(0)  #dummy 
+        d1.next=head
+        cur=d1
+        while cur.next:
+            prev=cur
+            if cur.next.val==0:
+                cur=cur.next
+            if cur.next:
+                cur.val+=cur.next.val
+                cur.next=cur.next.next
+        prev.next=None
+        return head
